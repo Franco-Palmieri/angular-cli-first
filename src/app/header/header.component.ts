da7c91102f5a988a0956fcd28ae9a52799db3a69
Ncesecrede <francescopm95@gmail.com>
2022-01-02 18:45:38 +0100

    ngFor

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  listNav: string[] = ['Home', 'About', 'Contacts']
   constructor() { }
 
   ngOnInit(): void {
