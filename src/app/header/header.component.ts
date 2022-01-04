276a5ced0d3e80696f4981fdbd44bfbfe04e17dd
Ncesecrede <francescopm95@gmail.com>
2022-01-04 02:02:57 +0100

    Methods and Forms

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +8,9 @@ import { Component, Input, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   listNav: string[] = ['Home', 'About', 'Contacts']
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
   }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  money: number;
+  selectField: FormControl = new FormControl();
+
+  form: FormGroup;
+
+  constructor(public fb: FormBuilder) { 
+
+    let money1 = 5;
+    let money2 = 10;
+    this.money = this.sum(money1, money2)
+    console.log(this.money);
+
+
+
+    this.form = fb.group({
+      'number1': ['',],
+      'number2': ['',]
+    })
+    
+  }
 
   ngOnInit(): void {
+  }
+
+  send():void{ 
+    let result = this.sum(this.form.controls['number1'].value, this.form.controls['number2'].value);
+    console.log(result)
+    
+  }
+
+
+  sum(n1: number, n2:number):number{
+      return n1 + n2;
   }
 
 }
